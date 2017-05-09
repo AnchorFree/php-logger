6421c9178ccb7d8961e8c07a453c4f69fa7753a2
Yaroslav Molochko <y.molochko@anchorfree.com>
2017-05-09 11:51:49 -0700

    Create files beforehand

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,11 @@
 export TEAM=${TEAM:-"default"}
 export APPLICATION=${APPLICATION:-"php"}
 
+# workaround until we get to named pipes
+touch /log/php-error.log
+touch /log/fpm-error.log
+touch /log/slow.log
+touch /log/app-error.json
+touch /log/app-access.json
+
 exec fluentd -c /fluentd/etc/$FLUENTD_CONF -p /fluentd/plugins $FLUENTD_OPT "$@"
