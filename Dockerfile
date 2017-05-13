1e76dc8878e173d54a006fd8b4cc81dbaf45be65
Yaroslav Molochko <onorua@gmail.com>
2017-05-12 19:38:10 -0700

    INFRA-2997 Named pipe (#1)
    * Wrappers
    
    * fluent user by default
    
    * Try another version

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,16 @@ FROM fluent/fluentd:v0.12
 
 COPY ./fluent.conf /fluentd/etc/fluent.conf
 COPY ./entrypoint.sh /entrypoint.sh
+COPY ./plugins/* /fluentd/plugins/
+USER root
+RUN apk add --update \
+    ruby-dev \
+    ruby-rake \
+    make \ 
+    gcc \
+    build-base 
+
+RUN gem install --no-user-install ruby-fifo -v 0.0.1
 
 ENTRYPOINT ["/entrypoint.sh"]
 CMD []
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 export TEAM=${TEAM:-"default"}
 export APPLICATION=${APPLICATION:-"php"}
 
-# workaround until we get to named pipes
-touch /log/php-error.log
-touch /log/fpm-error.log
-touch /log/slow.log
-touch /log/app-error.json
-touch /log/app-access.json
+# create named pipes before using them
+for i in php-error.log fpm-error.log slow.log app-error.json app-access.json
+do
+    if [[ ! -p "/log/${i}" ]]; then
+        mkfifo -m666 "/log/${i}"
+    fi
+done
 
 exec fluentd -c /fluentd/etc/$FLUENTD_CONF -p /fluentd/plugins $FLUENTD_OPT "$@"
