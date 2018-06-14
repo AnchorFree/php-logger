61263115b1d2aed1af4ad5d36aa18aa5714cadd4
Dmitriy Shmatov <d.shmatov@anchorfree.com>
2018-06-14 18:25:46 +0300

    Provide syslog-ng

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,6 @@
-FROM fluent/fluentd:v0.12
+FROM balabit/syslog-ng:3.14.1
 
-COPY ./fluent.conf /fluentd/etc/fluent.conf
 COPY ./entrypoint.sh /entrypoint.sh
-COPY ./plugins/* /fluentd/plugins/
-USER root
-RUN apk add --update \
-    ruby-dev \
-    ruby-rake \
-    make \
-    gcc \
-    build-base
-
-RUN gem install --no-user-install ruby-fifo -v 0.0.1
-RUN fluent-gem install fluent-plugin-map -v 0.1.3
 
 ENTRYPOINT ["/entrypoint.sh"]
 CMD []
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-export TEAM=${TEAM:-"default"}
-export APPLICATION=${APPLICATION:-"php"}
+if [[ ! -p "/log/slow.log" ]]; then
+    mkfifo -m666 "/log/slow.log"
+fi
 
-# create named pipes before using them
-for i in php-error.log fpm-error.log slow.log app-error.json app-access.json
-do
-    if [[ ! -p "/log/${i}" ]]; then
-        mkfifo -m666 "/log/${i}"
-    fi
-done
-
-exec fluentd -c /fluentd/etc/$FLUENTD_CONF -p /fluentd/plugins $FLUENTD_OPT "$@"
+exec /usr/sbin/syslog-ng -F "$@"
