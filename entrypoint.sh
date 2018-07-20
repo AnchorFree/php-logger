64d3d62970e06bcd1df89abf14e2be4f34178a2a
Dmitriy Shmatov <d.shmatov@anchorfree.com>
2018-07-20 15:20:54 +0300

    Move to files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-for i in php-error.log slow.log
+
+[[ ! -d /log  ]] && mkdir /log
+
+for i in php-error.log fpm-error.log slow.log app-error.json app-access.json
 do
-    if [[ ! -p "/log/${i}" ]]; then
-        rm -f /log/${i}
-        mkfifo -m666 "/log/${i}"
+    if [[ ! -f /log/${i} ]]; then
+        rm -fr /log/${i}
+        > /log/${i}
     fi
+    chmod 0666 /log/${i}
 done
 
 exec /usr/sbin/syslog-ng -F "$@"
